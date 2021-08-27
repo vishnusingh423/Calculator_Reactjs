ad608ff2073bd6ad4374ac9c9c2ae49abb52305b
Vishnu Singh <42887966+vishnusingh423@users.noreply.github.com>
2021-08-28 01:44:48 +0530

    Add files via upload
    Adding Components of calculator and fixing ui

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+import Calculator from "./Calculator";
+const App = () => {
+  return (
+    <div id="wrapper">
+      <div id="app">
+        <Calculator />
+      </div>
+    </div>
+  );
+};
+
+export default App;
